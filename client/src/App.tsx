cecf7c262878f7f60fea14a9a93cfd762fb44c5b
atharv <atharvnandurkar@gmail.com>
2025-10-19 13:19:41 +0000

    Navbar created

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import './App.css'
+import { Dashboard } from './components/Dashboard'
 
 function App() {
   return <div className='bg-black min-h-screen'>
-    
+    <Dashboard />
   </div>
 
 }
---
+++ client/src/components/Dashboard.tsx
@@ -0,0 +1,9 @@
+import { Navbar } from "./Navbar";
+
+export function Dashboard() {
+    return <div className="text-white">
+        <Navbar />
+        <hr className="mt-6 text-gray-700" />
+    </div>
+}
+
